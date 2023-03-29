7940164aabedb573a81026ca6bb27f0b66801fa7
Taylor Mansfield <lavahot@users.noreply.github.com>
2023-03-29 12:26:16 -0700

    Enable dns to fix dns issue

--- vpc.tf
+++ vpc.tf
@@ -15,6 +15,7 @@ module "vpc" {
   enable_nat_gateway      = true
   single_nat_gateway      = true
   enable_dns_hostnames    = true
+  enable_dns_support      = true
   map_public_ip_on_launch = false
 
   # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
